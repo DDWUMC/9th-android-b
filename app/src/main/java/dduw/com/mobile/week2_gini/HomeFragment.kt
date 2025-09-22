f473a1e8dc653e9356166d64eaf87ca07960ce57
SoyeonRyou <ryousoooo@gmail.com>
2025-09-22 19:56:32 +0900

    feat : Week2_gini 커밋완료

---
+++ app/src/main/java/dduw/com/mobile/week2_gini/HomeFragment.kt
@@ -0,0 +1,31 @@
+package dduw.com.mobile.week2_gini
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dduw.com.mobile.week2_gini.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.homeAlbumImgIv1.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm , AlbumFragment())
+                .commitAllowingStateLoss()
+        }
+
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/dduw/com/mobile/week2_gini/LockerFragment.kt
@@ -0,0 +1,23 @@
+package dduw.com.mobile.week2_gini
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dduw.com.mobile.week2_gini.databinding.FragmentLockerBinding
+
+class LockerFragment : Fragment() {
+
+    lateinit var binding: FragmentLockerBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLockerBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
